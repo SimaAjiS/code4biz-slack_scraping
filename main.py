b05cae4c576a12e561b366952c5be026eb0dc39f
SimaAjiS <comb.yasushi+github@gmail.com>
2022-07-04 22:11:53 +0900

    bugfix: 暫定版完成

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ import openpyxl
 import pandas as pd
 import pyautogui as pg
 import pyperclip
-from openpyxl.styles import Font
+from openpyxl.styles import Font, Alignment
 from openpyxl.worksheet.table import Table, TableStyleInfo
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -166,7 +165,7 @@ def get_contents_1message(message, search_results_count, start_time):
     # 長文の場合の”...もっと表示する”となっている場合の対応
     try:
         if len(message.find_elements(by=By.CLASS_NAME, value='c-search__expand_ellipsis')) > 0:
-            print('”...もっと表示する”あり')
+            print('\n”...もっと表示する”を展開します')
             message.find_element(by=By.CLASS_NAME, value='c-search__expand_ellipsis').click()
             sleep(1)
     except:
@@ -176,11 +175,36 @@ def get_contents_1message(message, search_results_count, start_time):
     except:
         try:
             text_section = get_text_section(message, class_name='c-search_message__body')
-        except:
+        except Exception as e:
+            print(e)
             print('除外処理')
             text_section = ''  # エクセルで検索しやすいように空白
     urls = message.find_elements(by=By.TAG_NAME, value='a')
     url = urls[1].get_attribute('href')
+    url2, url3, url4, url5, url6 = ['', '', '', '', '']
+    if len(urls) == 3:
+        url2 = urls[2].get_attribute('href')
+    elif len(urls) == 4:
+        url2 = urls[2].get_attribute('href')
+        url3 = urls[3].get_attribute('href')
+    elif len(urls) == 5:
+        url2 = urls[2].get_attribute('href')
+        url3 = urls[3].get_attribute('href')
+        url4 = urls[4].get_attribute('href')
+    elif len(urls) == 6:
+        url2 = urls[2].get_attribute('href')
+        url3 = urls[3].get_attribute('href')
+        url4 = urls[4].get_attribute('href')
+        url5 = urls[5].get_attribute('href')
+    elif len(urls) == 6:
+        url2 = urls[2].get_attribute('href')
+        url3 = urls[3].get_attribute('href')
+        url4 = urls[4].get_attribute('href')
+        url5 = urls[5].get_attribute('href')
+        url6 = urls[6].get_attribute('href')
+    else:
+        pass
+
     thread_ts = url.split('=')[-1]
     datum = {
         '検索結果数': search_results_count,
@@ -191,7 +215,12 @@ def get_contents_1message(message, search_results_count, start_time):
         '投稿チャンネル': channel_name,
         '投稿者': sender_name,
         '投稿メッセージ': text_section,
-        'リンク': url
+        'リンク': url,
+        'リンク2': url2,
+        'リンク3': url3,
+        'リンク4': url4,
+        'リンク5': url5,
+        'リンク6': url6
     }
     return datum, sender_name, text_section, timestamp
 
@@ -209,7 +238,7 @@ def excel_tabling(aggre_file_name):
     ws_summary = wb.worksheets[-1]
 
     # テーブルを生成する
-    table = Table(displayName='T集計', ref='A1:I200')
+    table = Table(displayName='T集計', ref='A1:N200')
 
     # テーブルのスタイルを決める(紫、テーブルスタイル(中間))
     table_style = TableStyleInfo(name='TableStyleMedium5', showRowStripes=True)
@@ -229,7 +258,12 @@ def excel_tabling(aggre_file_name):
     ws_summary.column_dimensions['F'].width = 25
     ws_summary.column_dimensions['G'].width = 20
     ws_summary.column_dimensions['H'].width = 50
-    ws_summary.column_dimensions['I'].width = 50
+    ws_summary.column_dimensions['I'].width = 15
+    ws_summary.column_dimensions['J'].width = 15
+    ws_summary.column_dimensions['K'].width = 15
+    ws_summary.column_dimensions['L'].width = 15
+    ws_summary.column_dimensions['M'].width = 15
+    ws_summary.column_dimensions['N'].width = 15
 
     # テーブル化したフォントの設定
     font_name = 'Meiryo'
@@ -247,7 +281,20 @@ def excel_tabling(aggre_file_name):
         ws_summary.cell(row=i, column=6).font = Font(name=font_name, sz=11)
         ws_summary.cell(row=i, column=7).font = Font(name=font_name, sz=11)
         ws_summary.cell(row=i, column=8).font = Font(name=font_name, sz=11)
-        ws_summary.cell(row=i, column=9).font = Font(name=font_name, sz=11)
+        ws_summary.cell(row=i, column=9).font = Font(name=font_name, sz=8)
+        ws_summary.cell(row=i, column=10).font = Font(name=font_name, sz=8)
+        ws_summary.cell(row=i, column=11).font = Font(name=font_name, sz=8)
+        ws_summary.cell(row=i, column=12).font = Font(name=font_name, sz=8)
+        ws_summary.cell(row=i, column=13).font = Font(name=font_name, sz=8)
+        ws_summary.cell(row=i, column=14).font = Font(name=font_name, sz=8)
+
+    for i in range(2, ws_summary.max_row + 1):
+        ws_summary.cell(row=i, column=9).alignment = Alignment(shrinkToFit=True)
+        ws_summary.cell(row=i, column=10).alignment = Alignment(shrinkToFit=True)
+        ws_summary.cell(row=i, column=11).alignment = Alignment(shrinkToFit=True)
+        ws_summary.cell(row=i, column=12).alignment = Alignment(shrinkToFit=True)
+        ws_summary.cell(row=i, column=13).alignment = Alignment(shrinkToFit=True)
+        ws_summary.cell(row=i, column=14).alignment = Alignment(shrinkToFit=True)
 
     wb.save(aggre_file_name)
 
@@ -279,26 +326,29 @@ def main(start, end):
         pg.press('enter', presses=1, interval=0.1)
         pg.press('down', presses=2, interval=0.2)
         pg.press('enter', presses=1, interval=0.1)
-        # 全件表示
+        # 20件表示
         pg.press('tab', presses=1, interval=0.2)
         pg.press('enter', presses=1, interval=0.1)
-        pg.press('down', presses=3, interval=0.2)
+        pg.press('down', presses=0, interval=0.2)
         pg.press('enter', presses=1, interval=0.1)
 
         # 表示を小さくする
         display_smaller()
 
         # 一番下まで移動
-        pg.press('tab', presses=3, interval=0.5)
-        pg.press('pagedown', presses=10, interval=0.5)
+        pg.press('end', presses=1, interval=0.5)
         sleep(2)
 
         # 取得日時
         start_time = dt.now()
 
-        # メッセージ重複有り（取得後、pandasにて重複削除）
-        roop_total = math.floor(search_results_count / 21)
-        for i in range(roop_total + 1):  # roop_total 回までスクロール
+        # 20件/ページ として総ページ算出
+        pages = math.ceil(search_results_count / 20)
+        print(f'計 {pages}ページ')
+
+        # メッセージ重複有り → 取得後pandasにて重複削除
+        for page in range(pages):
+            # 表示ページ内のメッセージグループを取得
             message_groups = driver.find_elements(by=By.CLASS_NAME, value='c-message_group')
             print(f'メッセージ数:{len(message_groups)}')
 
@@ -306,38 +356,43 @@ def main(start, end):
             message_groups.reverse()
 
             try:
-                for j, message in enumerate(message_groups):
+                for i, message in enumerate(message_groups):
                     try:
                         datum, sender_name, text_section, timestamp = get_contents_1message(
                             message, search_results_count, start_time)
                         data.append(datum)
                         print(
-                            f'\n{search_day} {i}/{roop_total}スクロール目{j + 1}/{len(message_groups)}件 (全{search_results_count}件):{sender_name} {timestamp}')
+                            f'\n{search_day} {page + 1}/{pages}ページ目{i + 1}/{len(message_groups)}件 (全{search_results_count}件):{sender_name} {timestamp}')
                         print(f'{text_section[:50]}...\nメッセージ取得完了')
                     except Exception as e:
                         # エラーによる取得NGの時スキップ処理
                         print(
-                            f'{search_day} {i}/{roop_total}スクロール目{j + 1}/{len(message_groups)}件 (全{search_results_count}件): 取得失敗！')
+                            f'{search_day} {page + 1}/{pages}ページ目{i + 1}/{len(message_groups)}件 (全{search_results_count}件): 取得失敗！')
                         print(e)
-                        # 待機時間（サイトに負荷を与えないと同時にコンテンツの読み込み待ち）
-                        sleep(0.5)
+
+                    # 待機時間（サイトに負荷を与えないと同時にコンテンツの読み込み待ち）
+                    sleep(0.5)
             except Exception as e:
                 print(e)
 
             # リスト反転もどす
             message_groups.reverse()
+            print(f'{page + 1}/{pages}ページの取得完了しました')
 
-            # 検索結果より1度の取得件数が少なければ、取得メッセージ先頭の要素までスクロール
-            if search_results_count > len(message_groups):
-                first_message = message_groups[0]
-                actions = ActionChains(driver)
-                actions.move_to_element(first_message)
-                actions.perform()
-                # 待機時間（サイトに負荷を与えないと同時にコンテンツの読み込み待ち）
-                sleep(3)
+            # 検索結果より1度の取得件数が少なければ、次のページクリック
+
+            if page + 1 < pages:
+                # 一番下まで移動
+                pg.press('end', presses=1, interval=0.5)
+                sleep(2)
+                try:
+                    next_button = driver.find_element(by=By.XPATH, value='//*[@aria-label="次のページ"]')
+                    next_button.click()
+                    print(f'\n{page + 2}へのボタンをクリックしました')
+                except Exception as e:
+                    print(e)
             else:
                 pass
-
             # 待機時間（サイトに負荷を与えないと同時にコンテンツの読み込み待ち）
             sleep(3)
 
@@ -368,8 +423,8 @@ def main(start, end):
 
 if __name__ == '__main__':
     # 期間指定
-    start = '2022-04-01'
-    end = '2022-04-01'
+    start = '2022-03-13'
+    end = '2022-03-13'
 
     main(start=start, end=end)
     print(f'{start}～{end}の全件取得完了')
