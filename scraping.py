3cd9e7859015db231a7b878d4014803757c4f368
SimaAjiS <comb.yasushi+github@gmail.com>
2022-07-10 15:07:55 +0900

    update : 7/9

--- scraping.py
+++ scraping.py
@@ -430,8 +430,8 @@ if __name__ == '__main__':
     # end = start
 
     # 期間指定
-    start = '2022-07-04'
-    end = '2022-07-06'
+    start = '2022-07-09'
+    end = '2022-07-09'
 
     main(start=start, end=end)
     print(f'{start}～{end}の全件取得完了')
